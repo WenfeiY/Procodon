6df35ea6760ce6a80a254319d95b2e1046243edb
Wenfei Yu <73346728+WenfeiY@users.noreply.github.com>
2026-04-21 14:15:15 +0800

    Delete 13-Decoding_data_from_NGS directory

--- 13-Decoding_data_from_NGS/03-Copying_contigs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-prefix=$1
-
-#  Copy contigs files to new folder
-bash Attached-09-cp_asm.sh $prefix
--- 13-Decoding_data_from_NGS/04-TBLASTN_contigs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-prefix=$1
-
-#  Align contigs with protein sequences
-bash Attached-11-TBLASTN_asm.sh $prefix
